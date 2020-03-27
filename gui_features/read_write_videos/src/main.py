55a7a90bd271c2132e5240ba049775ac6f5f8dc7
danbailo <danbailoufms@gmail.com>
2020-03-27 16:13:20 -0400

    success

--- gui_features/read_write_videos/src/main.py
+++ gui_features/read_write_videos/src/main.py
@@ -1,20 +1,17 @@
 #try with webcam
-
-import numpy as np
 import cv2 as cv
+import imutils
 
 cap = cv.VideoCapture("input/race.mp4")
 
 #print the widht and height of the video
 print(cap.get(cv.CAP_PROP_FRAME_WIDTH), cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
-
 # Define the codec and create VideoWriter object
-fourcc = cv.VideoWriter_fourcc('m', 'p', '4', 'v')
-fps = 20
+fourcc = cv.VideoWriter_fourcc(*"mp4v")
+fps = 30
 size = int(cap.get(cv.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-out = cv.VideoWriter()
-success = out.open('output/output.mp4', fourcc, fps, size, True)
+out = cv.VideoWriter('output/output.mp4', fourcc, fps, size, 0) #0 its for grayscale
 
 while cap.isOpened():
 	# Capture frame-by-frame
@@ -22,10 +19,9 @@ while cap.isOpened():
 
 	# if frame is read correctly ret is True
 	if not ret:
-		print("Can't receive frame (stream end?). Exiting ...")
 		break
+
 	# Our operations on the frame come here
-	
 	frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 	frame = cv.flip(frame, 0)
 	
@@ -33,6 +29,7 @@ while cap.isOpened():
 
 	# Display the resulting frame
 	cv.imshow('frame', frame)
+
 	if cv.waitKey(25) & 0xFF == ord('q'):
 		break	
 

c0644013d3a7adc801152128c76b2016d2736891
Daniel Bailo <danbailoufms@gmail.com>
2020-06-05 17:17:39 -0300

    dependencies to install opencv

---
+++ dependencies.sh
@@ -0,0 +1,7 @@
+sudo apt install \
+build-essential cmake unzip pkg-config \
+libjpeg-dev libpng-dev libtiff-dev \
+libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
+libxvidcore-dev libx264-dev \
+libgtk-3-dev \
+libatlas-base-dev gfortran -y

212c628e005e4a6f2687a125d0496a5a750ae7f8
Daniel Bailo <danbailoufms@gmail.com>
2020-06-06 13:31:47 -0300

    studying opencv by videos

--- dependencies.sh
+++
@@ -1,7 +0,0 @@
-sudo apt install \
-build-essential cmake unzip pkg-config \
-libjpeg-dev libpng-dev libtiff-dev \
-libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
-libxvidcore-dev libx264-dev \
-libgtk-3-dev \
-libatlas-base-dev gfortran -y

1a84f981f7caf15a85237ed9bece48f454b8b64b
Daniel Bailo <danbailoufms@gmail.com>
2020-06-08 22:15:08 -0300

    drawing when move mouse

---
+++ 1 - image_basics_with_opencv/3.1 - Drawing on image with mouse move.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+drawing = False
+ix, iy = -1, -1
+
+
+def draw_rectangle(event, x, y, flags, param):
+    global drawing, ix, iy
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix, iy = x, y
+    if event == cv2.EVENT_MOUSEMOVE:
+        if drawing:
+            cv2.rectangle(img, (ix, iy), (x, y), (255, 0, 0), -1)
+    if event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.rectangle(img, (ix, iy), (x, y), (255, 0, 0), -1)
+
+
+img = np.zeros((512, 512, 3))
+cv2.namedWindow(winname='my_drawing')
+cv2.setMouseCallback('my_drawing', draw_rectangle)
+
+while True: 
+    
+    cv2.imshow('my_drawing', img)
+    
+    if cv2.waitKey(20) & 0xFF == ord("q"):
+        break
+
+
+cv2.destroyAllWindows()
